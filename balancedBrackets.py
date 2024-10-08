158b13f77708a3092abfbf3788821d8501353e29
Ivan Matev <Ivan.Matev@Seattlecolleges.edu>
2024-10-08 13:41:47 -0700

    Balanced brakets complete with only minor problems

---
+++ balancedBrackets.py
@@ -0,0 +1,58 @@
+def has_balanced_brackets(phrase):
+    """
+    Does a given string have balanced pairs of brackets?
+    Given a string as input, return True or False depending on whether the
+    string contains balanced (), {}, [], and/or <>.
+    """
+
+    # The code currently breaks if there is any punctuation
+    
+    # This list will keep track of the next bracket that needs to close
+    nextToClose = []
+    # go through each character of the phrase
+    for bracket in phrase:
+        # if we are on a letter, skip it
+        if bracket.isalpha() or bracket == " ":
+            continue
+        # if the bracket is an opening bracket, add the closing braket to nextToClose
+        if bracket == "(":
+            nextToClose.append(")")
+            continue
+        if bracket == "{":
+            nextToClose.append("}")
+            continue
+        if bracket == "[":
+            nextToClose.append("]")
+            continue
+        if bracket == "<":
+            nextToClose.append(">")
+            continue
+        
+        # If there is a closing bracket, but no brackets are stored, return False 
+        # This happens when the string begins with a closing bracket and not an opening bracket
+        if len(nextToClose) > 0:
+            # Checks if the current closing bracket is equal to the last bracket saved in the array
+            if bracket == nextToClose[-1]:
+                # If correct bracket is present, remove from array and continue
+                nextToClose.pop()
+            # If the wrong bracket is found, the brackets are out of order. Return False
+            else:
+                return False
+        else:
+            print("Here")
+            return False
+    # Return true only if the length of the array is 0 meaning that all brackets have been closed
+    return not(len(nextToClose))
+
+# Test the code
+print(1, has_balanced_brackets("<[{()}]>")) # True
+print(2, has_balanced_brackets("<[{(yay)}]>")) # True
+print(3, has_balanced_brackets("{[[This has too many open square brackets.]}")) # False
+print(4, has_balanced_brackets("(This has {too many} ) closers. )")) # False
+print(5, has_balanced_brackets("There(is{text[between<most>of]the}brackets)to{see(if)the}code<can>handle it")) # True
+
+# Test edge cases
+print(6, has_balanced_brackets(" ")) # True
+print(7, has_balanced_brackets("")) # True
+print(8, has_balanced_brackets("<{([")) # False
+print(9, has_balanced_brackets("])}>")) # False
