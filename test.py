b44157194d5f556273998cc53352e095a0c2c5be
Ivan Matev <Ivan.Matev@Seattlecolleges.edu>
2024-10-08 13:48:19 -0700

    Changed the variable names and added more comments in the code

--- test.py
+++ test.py
@@ -1,21 +1,30 @@
-def vowelReplace(s):
+def vowelReplace(testString):
+  """
+  input: String
+  ouput: String
+
+  Approach:
+  Create a temp string variable
+  Go through the testString and check every letter to see if it's a vowel
+  If it is a vowel, change it to "*"
+  Return the modified temp string variable 
+  """
+
+  # create a temp variable to store the new string
   temp = ""
+  # create a list of vowels
   vowels = ['a','e','i','o','u','y']
-  for c in s:
-    if c.lower() in vowels:
+  # go through each character in the testString
+  for character in testString:
+    # if the lowercase character is found in the vowels list, add "*" to the return string
+    if character.lower() in vowels:
       temp += "*"
     else:
-      temp += c
+      # else add the current character to the return string
+      temp += character
+  # return the temp string after we get out of the loop
   return temp
 
-
+# Test cases
 print(vowelReplace("Ivan"))
 print(vowelReplace("Becca"))
-
-# Input: string
-# output: string
-
-# create a temp string variable
-# go through s and check every letter to see if it's a vowel
-# if it is a vowel, change the vowel to "*"
-# return the modified string
