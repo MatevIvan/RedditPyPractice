e7a72297af7169afe48d35da3d576bd44910cfdb
Ivan Matev <Ivan.Matev@Seattlecolleges.edu>
2024-10-10 10:19:54 -0700

    Small optimizations added

--- check.py
+++ check.py
@@ -7,35 +7,29 @@ def check(king, queen):
     """
     # ascii values for A-H are 65-72
 
-    goLeft = 0
-    goUp = 0
-    inBounds = True
-    # if king and queen are in the same row or aisle, return true
+    xDirection = 1
+    yDirection = 1
+    # if king and queen are in the same row or column, return true
     if (king[0] == queen[0]) or (king[1] == queen[1]):
         return True
     # find the general direction to check
     if king[0] < queen [0]:
-        goLeft = -1
-    else:
-        goLeft = 1
+        xDirection = -1
     if king[1] < queen[1]:
-        goUp = -1
-    else:
-        goUp = 1
+        yDirection = -1
 
     # set temp variable for the queen's coordinates so they can be changed
     queenLetter = ord(queen[0])
     queenNumber = (int)(queen[1])
-    while inBounds:
+    while True:
         if queenLetter == ord(king[0]) and queenNumber == (int)(king[1]):
             return True
         # change the queen's direction by 1 in the king's direction
-        queenLetter += goLeft
-        queenNumber += goUp
+        queenLetter += xDirection
+        queenNumber += yDirection
         # check if in bounds
         if queenLetter < 65 or queenLetter > 72 or queenNumber < 1 or queenNumber > 8:
             return False
-    return False
 
 # Test the Code
 print(check("D6","H6")) # True
